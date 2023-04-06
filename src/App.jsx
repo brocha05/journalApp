77aa57125e8f59d0a6d7597ad932d780a223bc86
Brocha05 <brocha_05@hotmail.com>
2023-04-06 08:54:28 -0600

    Setting up MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import { AppRouter } from './router/AppRouter'
+import React from "react";
+import { AppRouter } from "./router/AppRouter";
+import { AppTheme } from "./theme/AppTheme";
 
 export const App = () => {
   return (
-    <AppRouter/>
-  )
-}
+    <AppTheme>
+      <AppRouter />
+    </AppTheme>
+  );
+};
