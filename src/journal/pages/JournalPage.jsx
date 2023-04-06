be326d32d5109b1484653a5db44dd01a48b353e2
Brocha05 <brocha_05@hotmail.com>
2023-04-06 12:02:02 -0600

    NothingSelectedView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,14 +1,14 @@
 import { MailOutline } from "@mui/icons-material";
 import { Typography } from "@mui/material";
 import React from "react";
+import { NothingSelectedView } from "../../views/NothingSelectedView";
 import { JournalLayout } from "../layout/JournalLayout";
 
 export const JournalPage = () => {
   return (
     <>
       <JournalLayout>
-        <Typography component="h1">JournalPage</Typography>
-        <MailOutline />
+        <NothingSelectedView/>
       </JournalLayout>
     </>
   );
