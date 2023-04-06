3026ffef1731dd030d09d5310825ecb4de6ee3b4
Brocha05 <brocha_05@hotmail.com>
2023-04-06 12:41:12 -0600

    Image gallery

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { IconButton } from '@mui/material';
+import { AddOutlined } from '@mui/icons-material';
 import { NoteView } from "../../views/NoteView";
 import { NothingSelectedView } from "../../views/NothingSelectedView";
 import { JournalLayout } from "../layout/JournalLayout";
@@ -8,7 +9,21 @@ export const JournalPage = () => {
     <>
       <JournalLayout>
         {/* <NothingSelectedView/> */}
-        <NoteView/>
+        <NoteView />
+
+        <IconButton
+          size="large"
+          sx={{
+            color: "white",
+            backgroundColor: "error.main",
+            ":hover": { backgroundColor: "error.main", opacity: 0.9 },
+            position: "fixed",
+            right: 50,
+            bottom: 50,
+          }}
+        >
+          <AddOutlined sx={{ fontSize: 30 }} />
+        </IconButton>
       </JournalLayout>
     </>
   );
